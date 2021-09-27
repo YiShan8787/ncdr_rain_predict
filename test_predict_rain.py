adf4fb9c2c2520c94829cb7eaae37c67fe449d87
YiShan8787 <59713684gx@gmail.com>
2021-09-27 15:00:09 +0800

    test

--- test_predict_rain.py
+++ test_predict_rain.py
@@ -45,11 +45,12 @@ import os
 from openpyxl import load_workbook
 import time as tt
 
+from tensorflow import keras
+
 #os.environ["CUDA_VISIBLE_DEVICES"] = "" # use cpu
 # construct the argument parser and parse the arguments
 
 ###############################################################################
-#----------------------PATH--------------------
 ap = argparse.ArgumentParser()
 # weather image path
 ap.add_argument("-wd", "--weather_dataset", required=False,
@@ -57,11 +58,16 @@ ap.add_argument("-wd", "--weather_dataset", required=False,
 ap.add_argument("-sd", "--satellite_dataset", required=False,
 	help="path to input dataset", default = "/media/ubuntu/My Passport/NCDR/ncdr_rain_predict/data/satellite")
 # ground truth path
-ap.add_argument("-gt", "--gt", type=str, default="data/gt/south.xlsx",
+ap.add_argument("-gt", "--gt", type=str, default="data/gt/north.xlsx",
 	help="gt for the data")
+
 station_path = '/media/ubuntu/My Passport/NCDR/ncdr_rain_predict/data/station_data'
 
-#----------------trainning parameters--------
+model_path = "/media/ubuntu/My Passport/NCDR/ncdr_rain_predict/Model/north/1632038787.8442829.h5"
+# station data time
+ap.add_argument("-st", "--station_time", type=int, default=12,
+	help="time for station data")
+args = vars(ap.parse_args())
 # initialize the initial learning rate, number of epochs to train for,
 # and batch size
 INIT_LR = 1e-3
@@ -70,27 +76,20 @@ BS = 1
 num_folds = 3
 
 random_st = 42
+use_sampling = False
 
-#-----------preprocessing parameters---------
-#using sampling to balance the dataset T:F = 1:1
-use_sampling = True
-#satellite input frame, default = last 10 frames
+
 satellite_frame = -10
-# station data time, default is 12 which is means 0:00 ~11:00
-ap.add_argument("-st", "--station_time", type=int, default=12,
-	help="time for station data")
 
-#list of special staion input
+
+
+
 #north '466900','466940','466920'
 #mid 'C0G860','C01460'??
 #south 'C0V250','01O760'??,'C0R140'
-
-special_station_input_id = ['C0V250','C0R140']
+special_station_input_id = ['466900','466940','466920']
 
 #################################################################################
-
-satellite_x = 210
-satellite_y = 340
 
 # loss and acc file name
 ap.add_argument("-p", "--plot", type=str, default="loss_acc.png",
@@ -99,7 +98,9 @@ ap.add_argument("-p", "--plot", type=str, default="loss_acc.png",
 ap.add_argument("-m", "--model", type=str, default="rain_predict.h5",
 	help="path to output loss/accuracy plot")
 
-args = vars(ap.parse_args())
+satellite_x = 210
+satellite_y = 340
+
 print("[INFO] loading gt")
 gtPaths = args["gt"]
 station_time = args["station_time"]
@@ -452,282 +453,18 @@ if use_sampling:
     print("shape of special stations ", data_special_stations.shape)
 
 
-print("[INFO] train-test split")
+print("[INFO] load model")
 
-(train_weather_X, test_weather_X, train_weather_Y, test_weather_Y, index_train, index_test) = train_test_split(data_weathers, category_labels, range(data_weathers.shape[0]),
-	test_size=0.20, stratify=category_labels, random_state=random_st)
-#for i in range(len(train_weather_Y)):
-#    if np.argmax(train_weather_Y[i]) == 1:
- #       print("there is positive set in training set")
-  #      break
-print("train_weather shape: ", train_weather_X.shape)
-del data_weathers
-print("finish split weather")
 
-(train_temp_X, test_temp_X, train_temp_Y, test_temp_Y) = train_test_split(data_station_temperature, category_labels,
-	test_size=0.20, stratify=category_labels, random_state=random_st)
 
-del  train_temp_Y
-del  test_temp_Y
-
-del data_station_temperature
-print("finish split temp")
-
-(train_huminity_X, test_huminity_X, train_huminity_Y, test_huminity_Y) = train_test_split(data_station_huminity, category_labels,
-	test_size=0.20, stratify=category_labels, random_state=random_st)
-
-del train_huminity_Y
-del test_huminity_Y
-
-del data_station_huminity
-print("finish split huminity")
-
-(train_wind_X, test_wind_X, train_wind_Y, test_wind_Y) = train_test_split(data_station_wind_direction, category_labels,
-	test_size=0.20, stratify=category_labels, random_state=random_st)
-
-del train_wind_Y
-del test_wind_Y
-
-del data_station_wind_direction
-print("finish split wind")
-
-(train_satellite_X, test_satellite_X, train_satellite_Y, test_satellite_Y) = train_test_split(data_satellite, category_labels,
-	test_size=0.20, stratify=category_labels, random_state=random_st)
-
-del train_satellite_Y
-del test_satellite_Y
-
-del data_satellite
-print("finish split satellite")
-
-(train_special_stations_X, test_special_stations_X, train_special_stations_Y, test_special_stations_Y) = train_test_split(data_special_stations, category_labels,
-	test_size=0.20, stratify=category_labels, random_state=random_st)
-
-del train_special_stations_Y
-del test_special_stations_Y
-
-del data_special_stations
-print("finish split satellite")
-
-
-print("[INFO] build model")
-
-weather_frames, weather_channels, station_frames, station_channels, rows, columns = 1,3, station_time, 3,210,340
-
-#encode model
-
-weather_video = Input(shape=(weather_frames,
-                     columns,
-                     rows,
-                     weather_channels))
-
-temp_video = Input(shape=(station_frames,
-                     rows,
-                     columns,
-                     station_channels))
-
-huminity_video = Input(shape=(station_frames,
-                     rows,
-                     columns,
-                     station_channels))
-
-wind_video = Input(shape=(station_frames,
-                rows,
-                columns,
-                station_channels))      
-
-satellite_video = Input(shape=(abs(satellite_frame),
-                satellite_x,
-                satellite_y,
-                station_channels))
-
-special_stations_video = Input(shape=(station_frames,
-                len(special_station_input_id)*3,
-                )
-)       
-
-#vgg model
-
-vgg_weather = VGG16(input_shape=(rows,
-                              columns,
-                              weather_channels),
-                 weights="imagenet",
-                 include_top=False)
-vgg_weather.trainable = False
-
-vgg_temp = VGG16(input_shape=(rows,
-                              columns,
-                              station_channels),
-                 weights="imagenet",
-                 include_top=False)
-vgg_temp.trainable = False
-
-vgg_huminity = VGG16(input_shape=(rows,
-                              columns,
-                              station_channels),
-                 weights="imagenet",
-                 include_top=False)
-vgg_huminity.trainable = False
-
-vgg_wind = VGG16(input_shape=(rows,
-                              columns,
-                              station_channels),
-                 weights="imagenet",
-                 include_top=False)
-vgg_wind.trainable = False
-
-vgg_satellite = VGG16(input_shape=(satellite_x,
-                              satellite_y,
-                              station_channels),
-                 weights="imagenet",
-                 include_top=False)
-vgg_satellite.trainable = False
-
-conv_1d_special_stations = Conv1D(
-                filters=32,
-               kernel_size=8,
-               strides=1,
-               activation='relu')(special_stations_video)
-
-#cnn out
-
-cnn_out_weather = GlobalAveragePooling2D()(vgg_weather.output)
-
-cnn_out_temp = GlobalAveragePooling2D()(vgg_temp.output)
-
-cnn_out_huminity = GlobalAveragePooling2D()(vgg_huminity.output)
-
-cnn_out_wind = GlobalAveragePooling2D()(vgg_wind.output)
-
-cnn_out_satellite = GlobalAveragePooling2D()(vgg_satellite.output)
-
-cnn_out_special_stations = MaxPooling1D(pool_size=4)(conv_1d_special_stations)
-
-#cnn model 
-
-cnn_weather = Model(vgg_weather.input, cnn_out_weather)
-
-cnn_temp = Model(vgg_temp.input, cnn_out_temp)
-
-cnn_huminity = Model(vgg_huminity.input, cnn_out_huminity)
-
-cnn_wind = Model(vgg_wind.input, cnn_out_wind)
-
-cnn_satellite = Model(vgg_satellite.input, cnn_out_satellite)
-
-cnn_special_stations = Model(special_stations_video, cnn_out_special_stations)
-
-#encode frame
-
-weather_encoded_frames = TimeDistributed(cnn_weather)(weather_video)
-
-temp_encoded_frames = TimeDistributed(cnn_temp)(temp_video)
-
-huminity_encoded_frames = TimeDistributed(cnn_huminity)(huminity_video)
-
-wind_encoded_frames = TimeDistributed(cnn_wind)(wind_video)
-
-satellite_encoded_frames = TimeDistributed(cnn_satellite)(satellite_video)
-
-#special_stations_encoded_frames = TimeDistributed(cnn_special_stations)(special_stations_video)
-
-# LSTM
-
-weather_encoded_sequence = LSTM(256)(weather_encoded_frames)
-
-temp_encoded_sequence = LSTM(256)(temp_encoded_frames)
-
-huminity_encoded_sequence = LSTM(256)(huminity_encoded_frames)
-
-wind_encoded_sequence = LSTM(256)(wind_encoded_frames)
-
-satellite_encoded_sequence = LSTM(256)(satellite_encoded_frames)
-
-special_stations_encodeed_sequence = LSTM(256)(cnn_out_special_stations)
-
-#concate
-
-encoded_sequence = concatenate([
-    weather_encoded_sequence, 
-    temp_encoded_sequence, 
-    huminity_encoded_sequence, 
-    wind_encoded_sequence, 
-    satellite_encoded_sequence, 
-    special_stations_encodeed_sequence])
-
-# dense layer
-
-hidden_layer = Dense(1024, activation="relu")(encoded_sequence)
-outputs = Dense(2, activation="softmax")(hidden_layer)
-
-# build all model
-
-model = Model(inputs = [weather_video, temp_video, huminity_video, wind_video, satellite_video,special_stations_video], outputs= [outputs])
+model = keras.models.load_model(model_path)
 model.summary()
 
 # compile our model
-print("[INFO] compiling model...")
-opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-model.compile(loss="binary_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
-# train the head of the network
-print("[INFO] training head...")
-
-# Define per-fold score containers
-acc_per_fold = []
-loss_per_fold = []
-
-# Merge inputs and targets
-#inputs_set = np.concatenate((train_weather_X, test_weather_X), axis=0)
-#targets_set = np.concatenate((train_weather_Y, test_weather_Y), axis=0)
-
-# Define the K-fold Cross Validator
-kfold = KFold(n_splits=num_folds, shuffle=True)
-
-# K-fold Cross Validation model evaluation
-fold_no = 1
-
-for train_index, test_index in kfold.split(train_weather_X, train_weather_Y):
-    print("TRAIN:", train_index, "TEST:", test_index)
-
-    X_weather_train, X_weather_test = train_weather_X[train_index], train_weather_X[test_index]
-    X_temp_train, X_temp_test = train_temp_X[train_index], train_temp_X[test_index]
-    X_huminity_train, X_huminity_test = train_huminity_X[train_index], train_huminity_X[test_index]
-    X_wind_train, X_wind_test = train_wind_X[train_index], train_wind_X[test_index]
-    X_satellite_train, X_satellite_test = train_satellite_X[train_index], train_satellite_X[test_index]
-    X_special_stations_train, X_special_stations_test = train_special_stations_X[train_index], train_special_stations_X[test_index]
-
-    Y_train, Y_test = train_weather_Y[train_index], train_weather_Y[test_index]
-
-    print('------------------------------------------------------------------------')
-    print(f'Training for fold {fold_no} ...')
-
-    # Fit data to model
-    history = model.fit([X_weather_train, 
-                X_temp_train, 
-                X_huminity_train, 
-                X_wind_train, 
-                X_satellite_train, 
-                X_special_stations_train], Y_train,
-                batch_size=BS,
-                epochs=EPOCHS,
-                verbose=1)
-
-    # Generate generalization metrics
-    scores = model.evaluate([X_weather_test, 
-    X_temp_test, 
-    X_huminity_test, 
-    X_wind_test, 
-    X_satellite_test, 
-    X_special_stations_test], 
-    train_weather_Y[test_index], verbose=0,batch_size = BS)
-
-    print(f'Score for fold {fold_no}: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%')
-    acc_per_fold.append(scores[1] * 100)
-    loss_per_fold.append(scores[0])
-
-    # Increase fold number
-    fold_no = fold_no + 1
+#print("[INFO] compiling model...")
+#opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+#model.compile(loss="binary_crossentropy", optimizer=opt,
+#	metrics=["accuracy"])
 
 # == Provide average scores ==
 second = str(tt.time())
@@ -735,35 +472,18 @@ second = str(tt.time())
 f_log = open(second+".txt", "w")
 print('------------------------------------------------------------------------')
 f_log.write('------------------------------------------------------------------------')
-print('Score per fold')
-f_log.write('Score per fold')
-for i in range(0, len(acc_per_fold)):
-  print('------------------------------------------------------------------------')
-  f_log.write('------------------------------------------------------------------------')
-  print(f'> Fold {i+1} - Loss: {loss_per_fold[i]} - Accuracy: {acc_per_fold[i]}%')
-  f_log.write(f'> Fold {i+1} - Loss: {loss_per_fold[i]} - Accuracy: {acc_per_fold[i]}%')
-print('------------------------------------------------------------------------')
-print('Average scores for all folds:')
-print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
-print(f'> Loss: {np.mean(loss_per_fold)}')
-print('------------------------------------------------------------------------')
 
-f_log.write('------------------------------------------------------------------------')
-f_log.write('Average scores for all folds:')
-f_log.write(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
-f_log.write(f'> Loss: {np.mean(loss_per_fold)}')
-f_log.write('------------------------------------------------------------------------')
 
 
 # make predictions on the testing set
 print("[INFO] evaluating network...")
 f_log.write("[INFO] evaluating network...")
-predIdxs = model.predict([test_weather_X, 
-test_temp_X, 
-test_huminity_X, 
-test_wind_X, 
-test_satellite_X,
-test_special_stations_X], batch_size=BS)
+predIdxs = model.predict([data_weathers, 
+data_station_temperature, 
+data_station_huminity, 
+data_station_wind_direction, 
+data_satellite,
+data_special_stations], batch_size=BS)
 
 
 # for each image in the testing set we need to find the index of the
@@ -773,15 +493,15 @@ predIdxs = np.argmax(predIdxs, axis=1)
 #print("test index of true")
 #print(labels[np.where(np.argmax(test_weather_Y, axis=1)==1)])
 #f_log(labels[np.where(np.argmax(test_weather_Y, axis=1)==1)])
-print(classification_report(test_weather_Y.argmax(axis=1), predIdxs,
+print(classification_report(category_labels.argmax(axis=1), predIdxs,
 	target_names=['False', 'True']))
 
-f_log.write(classification_report(test_weather_Y.argmax(axis=1), predIdxs,
+f_log.write(classification_report(category_labels.argmax(axis=1), predIdxs,
 	target_names=['False', 'True']))
 
 # compute the confusion matrix and and use it to derive the raw
 # accuracy, sensitivity, and specificity
-cm = confusion_matrix(test_weather_Y.argmax(axis=1), predIdxs)
+cm = confusion_matrix(category_labels.argmax(axis=1), predIdxs)
 total = sum(sum(cm))
 acc = (cm[0, 0] + cm[1, 1]) / total
 sensitivity = cm[0, 0] / (cm[0, 0] + cm[0, 1])
@@ -802,31 +522,6 @@ f_log.write("specificity: {:.4f}".format(specificity))
 
 
 
-# plot the training loss and accuracy
-#print(history.history.keys())
-N = EPOCHS
-plt.style.use("ggplot")
-plt.figure()
-plt.plot(np.arange(0, N), history.history["loss"], label="train_loss")
-#plt.plot(np.arange(0, N), history.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, N), history.history["acc"], label="train_acc")
-#plt.plot(np.arange(0, N), history.history["val_accuracy"], label="val_acc")
-plt.title("Training Loss and Accuracy")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend(loc="lower left")
-plt.savefig(args["plot"])
-
-
-print(second)
-'''
-if acc >= 0.6 or history.history["loss"][-1]<0.5:
-    plt.savefig(second + '.png')
-    model.save(second + '.h5')
-'''
-# serialize the model to disk
-print("[INFO] saving model...")
-model.save(args["model"])
 
 f_log.close()
 
